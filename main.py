cd14a850264b96fcf333e57be29164f3f1316d82
yjoon0110 <127302889+yjoon0110@users.noreply.github.com>
2025-05-23 16:19:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return 'Welcome to Python Flask World'
+    return 'Welcome to Python Flask World 1.0 for automation'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
